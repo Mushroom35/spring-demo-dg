c000c27069633f98461dfd3e2ca9021ce41e89a3
Grzybek <grzybekdawid@gmail.com>
2021-02-20 13:37:04 +0000

    [add] - mvc structure + simple endpoint added

--- src/main/java/pl/sda/javalondek4springdemo/controller/MyFirstRestController.java
+++ src/main/java/pl/sda/javalondek4springdemo/controller/MyFirstRestController.java
@@ -4,12 +4,22 @@ package pl.sda.javalondek4springdemo.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import pl.sda.javalondek4springdemo.service.MyService;
+
+import java.util.Locale;
 
 @RestController
 public class MyFirstRestController {
 
     private static final Logger logger = LoggerFactory.getLogger(MyFirstRestController.class);
+
+    private final MyService myService;
+
+    public MyFirstRestController(MyService myService) {
+        this.myService = myService;
+    }
 
     @GetMapping("/say-something")
     public String saySomething() {
@@ -24,5 +34,14 @@ public class MyFirstRestController {
         logger.debug("is it visible???");
         logger.trace("and this??");
         return "hello rest:P";
+    }
+
+    // /convert-name?myName=Mariusz
+    @GetMapping("/convert-name")
+    public String convertMyNameToUpperCae(@RequestParam("myName") String name) {
+        String result = myService.convertToUpperCase(name);
+
+        logger.info("input param: [{}], result: [{}]", name, result);
+        return result;
     }
 }
