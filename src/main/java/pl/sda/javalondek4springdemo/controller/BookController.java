58af3a5e2b3769b6d2e5dfc85cb845de4883b582
Grzybek <grzybekdawid@gmail.com>
2021-03-07 12:06:40 +0000

    [change] - refactoring BookController class

--- src/main/java/pl/sda/javalondek4springdemo/controller/BookController.java
+++ src/main/java/pl/sda/javalondek4springdemo/controller/BookController.java
@@ -2,21 +2,19 @@ package pl.sda.javalondek4springdemo.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import pl.sda.javalondek4springdemo.dto.ExceptionResponse;
+import pl.sda.javalondek4springdemo.exception.BookNotFoundException;
 import pl.sda.javalondek4springdemo.model.Book;
 import pl.sda.javalondek4springdemo.service.BookService;
 
+import javax.servlet.http.HttpServletRequest;
 import java.net.URI;
 
+import java.time.Clock;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
