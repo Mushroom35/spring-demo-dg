2e43e8b1e34ce64e6d6bda6afe07722448f9dd73
Grzybek <grzybekdawid@gmail.com>
2021-03-07 11:57:55 +0000

    [add] - add response codes and exceptions for controller

--- src/main/java/pl/sda/javalondek4springdemo/controller/BookController.java
+++ src/main/java/pl/sda/javalondek4springdemo/controller/BookController.java
@@ -2,20 +2,18 @@ package pl.sda.javalondek4springdemo.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import pl.sda.javalondek4springdemo.dto.ExceptionResponse;
+import pl.sda.javalondek4springdemo.exception.BookNotFoundException;
 import pl.sda.javalondek4springdemo.model.Book;
 import pl.sda.javalondek4springdemo.service.BookService;
 
+import javax.servlet.http.HttpServletRequest;
 import java.net.URI;
+import java.time.Clock;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -59,7 +57,7 @@ public class BookController {
         var newBook = bookService.saveBook(toSave);
 
         return ResponseEntity.created(URI.create("books/" + newBook.getId()))
-            .body(newBook);
+                .body(newBook);
     }
 
     @DeleteMapping("/{id}")
@@ -77,6 +75,7 @@ public class BookController {
     }
 
     // update (replace)
+
     @PutMapping("/{id}")
     public Book replaceBook(@PathVariable("id") Long id, @RequestBody Book toReplace) {
         logger.info("replacing book with id: [{}] with new one: [{}]", id, toReplace);
@@ -90,5 +89,28 @@ public class BookController {
         logger.info("updating book with id: [{}] with new attributes: [{}]", id, toUpdate);
 
         return bookService.updateBookWithAttributes(id, toUpdate);
+    }
+
+//       try {
+//       all methods from current controller
+//    } catch (BookNotFoundException exception) {
+//
+//    } catch (AnotherException exc) {
+//
+//    }
+    // only for this controller
+
+    @ExceptionHandler(BookNotFoundException.class)
+    public ResponseEntity<ExceptionResponse> handleBookNotFoundException(BookNotFoundException exception, HttpServletRequest request) {
+        logger.warn("some unexpected exception has occurred :)", exception);
+
+        return ResponseEntity.badRequest().body(
+                new ExceptionResponse(LocalDateTime.now(Clock.systemUTC()),
+                        HttpStatus.BAD_REQUEST.value(),
+                        exception.getClass().getName(),
+                        exception.getMessage(),
+                        request.getServletPath()
+                )
+        );
     }
 }
